5a834f4503cb1eb2247fda1a75203e51f4782691
Lukáš Pevný <lukas.pevny987@gmail.com>
2021-11-27 14:31:48 +0100

    Database and repository instantiation

---
+++ app/src/main/java/cz/utb/fai/roomwordsample/WordsApplication.kt
@@ -0,0 +1,12 @@
+package cz.utb.fai.roomwordsample
+
+import android.app.Application
+import cz.utb.fai.roomwordsample.db.WordRepository
+import cz.utb.fai.roomwordsample.db.WordRoomDatabase
+
+class WordsApplication : Application() {
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
